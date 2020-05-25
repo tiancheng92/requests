e4e666650d55483fe65299a13368a6519f4aedb8
徐天成 <1205124941@qq.com>
2020-05-25 18:56:28 +0800

    upload

---
+++ Requests.go
@@ -0,0 +1,73 @@
+package requests
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/tidwall/gjson"
+	"github.com/wxnacy/wgo/arrays"
+)
+
+// Request 发起请求的结构体
+type Request struct {
+	Method string
+	URL    string
+	Data   string
+	Header map[string]string
+}
+
+// Response request请求的返回结果
+type Response struct {
+	StatusCode int
+	Body       string
+	Header     http.Header
+}
+
+// Run 执行request请求
+func (r Request) Run() (res Response, err error) {
+	var req *http.Request
+	// 判断方法是否合法
+	if arrays.Contains([]string{"OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT", "PATCH"}, r.Method) < 0 {
+		return Response{}, errors.New("invalid method")
+	}
+	// 判断URL是否为空
+	if r.URL == "" {
+		return Response{}, errors.New("URL is empty")
+	}
+	// 判断有无请求包体，如有则判断其格式是否为json
+	if r.Data != "" {
+		if !gjson.Valid(r.Data) {
+			return Response{}, errors.New("invalid json")
+		}
+		req, err = http.NewRequest(r.Method, r.URL, bytes.NewBuffer([]byte(r.Data)))
+	} else {
+		req, err = http.NewRequest(r.Method, r.URL, nil)
+	}
+	if err != nil {
+		return
+	}
+	// 设置请求头,如无则添加默认请求头
+	if len(r.Header) > 0 {
+		for k, v := range r.Header {
+			req.Header.Set(k, v)
+		}
+	} else {
+		req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	}
+	req.Close = true
+	// 开始请求
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	// 读取内容
+	body, _ := ioutil.ReadAll(resp.Body)
+	res.StatusCode = resp.StatusCode
+	res.Body = string(body)
+	res.Header = resp.Header
+	return
+}
