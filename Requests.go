118830c686cea7dbc24d0b6263c09bba0b05f231
徐天成 <xutiancheng@hupu.com>
2020-08-02 15:42:56 +0800

    update

--- Requests.go
+++ Requests.go
@@ -4,18 +4,19 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/wxnacy/wgo/arrays"
+	"time"
 )
 
 // Request 发起请求的结构体
 type Request struct {
-	Method string
-	URL    string
-	Data   string
-	Header map[string]string
+	Method  string
+	URL     string
+	Data    string
+	Header  map[string]string
+	TimeOut time.Duration
 }
 
 // Response request请求的返回结果
@@ -23,6 +24,10 @@ type Response struct {
 	StatusCode int
 	Body       string
 	Header     http.Header
+}
+
+func New() Request {
+	return Request{}
 }
 
 func (res Response) JsonBind(obj interface{}) error {
@@ -49,73 +54,92 @@ func (r *Request) AddHeader(key, value string) *Request {
 	return r
 }
 
-func (r *Request) Options() (res Response, err error) {
+func (r *Request) SetTimeOut(duration time.Duration) *Request {
+	r.TimeOut = duration
+	return r
+}
+
+func (r *Request) Options() (Response, error) {
 	r.Method = "OPTIONS"
 	return r.Run()
 }
 
-func (r *Request) Get() (res Response, err error) {
+func (r *Request) Get() (Response, error) {
 	r.Method = "GET"
 	return r.Run()
 }
 
-func (r *Request) Head() (res Response, err error) {
+func (r *Request) Head() (Response, error) {
 	r.Method = "HEAD"
 	return r.Run()
 }
 
-func (r *Request) Post() (res Response, err error) {
+func (r *Request) Post() (Response, error) {
 	r.Method = "POST"
 	return r.Run()
 }
 
-func (r *Request) Put() (res Response, err error) {
+func (r *Request) Put() (Response, error) {
 	r.Method = "PUT"
 	return r.Run()
 }
 
-func (r *Request) Delete() (res Response, err error) {
+func (r *Request) Delete() (Response, error) {
 	r.Method = "DELETE"
 	return r.Run()
 }
 
-func (r *Request) Trace() (res Response, err error) {
+func (r *Request) Trace() (Response, error) {
 	r.Method = "TRACE"
 	return r.Run()
 }
 
-func (r *Request) Connect() (res Response, err error) {
+func (r *Request) Connect() (Response, error) {
 	r.Method = "CONNECT"
 	return r.Run()
 }
 
-func (r *Request) Patch() (res Response, err error) {
+func (r *Request) Patch() (Response, error) {
 	r.Method = "PATCH"
 	return r.Run()
 }
 
-// Run 执行request请求
-func (r Request) Run() (res Response, err error) {
-	var req *http.Request
+func (r *Request) check() error {
 	// 判断方法是否合法
-	if arrays.Contains([]string{"OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT", "PATCH"}, r.Method) < 0 {
-		return Response{}, errors.New("invalid method")
+	var methodList = []string{"OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT", "PATCH"}
+	var methodValid = false
+	for i := 0; i < len(methodList); i++ {
+		if methodList[i] == r.Method {
+			methodValid = true
+			break
+		}
+	}
+	if !methodValid {
+		return errors.New("invalid method")
 	}
 	// 判断URL是否为空
 	if r.URL == "" {
-		return Response{}, errors.New("URL is empty")
+		return errors.New("URL is empty")
 	}
 	// 判断有无请求包体，如有则判断其格式是否为json
-	if r.Data != "" {
-		if !json.Valid([]byte(r.Data)) {
-			return Response{}, errors.New("invalid json")
-		}
-		req, err = http.NewRequest(r.Method, r.URL, bytes.NewBuffer([]byte(r.Data)))
-	} else {
-		req, err = http.NewRequest(r.Method, r.URL, nil)
+	if r.Data != "" && !json.Valid([]byte(r.Data)) {
+		return errors.New("invalid json")
 	}
+	return nil
+}
+
+func (r *Request) getHttpRequest() (*http.Request, error) {
+	var data io.Reader
+
+	if r.Data != "" {
+		data = bytes.NewBuffer([]byte(r.Data))
+	} else {
+		data = nil
+	}
+
+	req, err := http.NewRequest(r.Method, r.URL, data)
 	if err != nil {
-		return
+		return nil, err
 	}
 	// 设置请求头,如无则添加默认请求头
 	if len(r.Header) > 0 {
@@ -126,17 +150,41 @@ func (r Request) Run() (res Response, err error) {
 		req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	}
 	req.Close = true
+	return req, nil
+}
+
+// Run 执行request请求
+func (r Request) Run() (Response, error) {
+	// 检测数据
+	if err := r.check(); err != nil {
+		return Response{}, err
+	}
+
+	// 把数据绑定到http.request对象中
+	req, err := r.getHttpRequest()
+	if err != nil {
+		return Response{}, err
+	}
+
 	// 开始请求
-	client := &http.Client{}
+	client := &http.Client{Timeout: r.TimeOut}
 	resp, err := client.Do(req)
 	if err != nil {
-		return
+		return Response{}, err
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
 	// 读取内容
-	body, _ := ioutil.ReadAll(resp.Body)
-	res.StatusCode = resp.StatusCode
-	res.Body = string(body)
-	res.Header = resp.Header
-	return
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Response{}, err
+	}
+
+	return Response{
+		StatusCode: resp.StatusCode,
+		Body:       string(body),
+		Header:     resp.Header,
+	}, nil
 }
--- example/main.go
+++ example/main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"log"
 	"requests"
+	"time"
 )
 
 func ChainCall() {
 	var r requests.Request
-	res, err := r.SetUrl("").
-		SetData("").
-		AddHeader("", "").
-		Post()
+	res, err := r.SetUrl("http://baidu.com").Get()
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -21,8 +19,8 @@ func ChainCall() {
 
 func StructCall() {
 	res, err := requests.Request{
-		Method: "",
-		URL:    "",
+		Method: "GET",
+		URL:    "http://baidu.com",
 		Data:   "",
 		Header: nil,
 	}.Run()
@@ -30,24 +28,86 @@ func StructCall() {
 		log.Fatal(err)
 		return
 	}
-	fmt.Printf("%#v", res)
+	fmt.Printf("%#v\n", res)
 }
 
 func StructureCall() {
 	var r requests.Request
-	r.SetMethod("")
-	r.SetUrl("")
-	r.AddHeader("", "")
+	r.SetMethod("GET")
+	r.SetUrl("http://baidu.com")
 	// ...
 	res, err := r.Run()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Printf("%#v", res)
+	fmt.Printf("%#v\n", res)
 }
+
+type UserInfo struct {
+	Count    int64       `json:"count"`
+	Next     interface{} `json:"next"`
+	Previous interface{} `json:"previous"`
+	Results  []struct {
+		CellPhone    string        `json:"cell_phone"`
+		DateJoined   string        `json:"date_joined"`
+		Email        string        `json:"email"`
+		ExtendUserID string        `json:"extend_user_id"`
+		GroupList    []interface{} `json:"group_list"`
+		Groups       []interface{} `json:"groups"`
+		ID           int64         `json:"id"`
+		IsActive     bool          `json:"is_active"`
+		IsStaff      bool          `json:"is_staff"`
+		IsSuperuser  bool          `json:"is_superuser"`
+		LastLogin    string        `json:"last_login"`
+		LastName     string        `json:"last_name"`
+		Leader       struct {
+			LeaderID   int64  `json:"leader_id"`
+			LeaderName string `json:"leader_name"`
+		} `json:"leader"`
+		Remark interface{} `json:"remark"`
+		Team   struct {
+			Alias     string `json:"alias"`
+			ID        int64  `json:"id"`
+			Name      string `json:"name"`
+			Structure int64  `json:"structure"`
+		} `json:"team"`
+		UpdatedAt   int64  `json:"updated_at"`
+		User        int64  `json:"user"`
+		Username    string `json:"username"`
+		Userprofile int64  `json:"userprofile"`
+	} `json:"results"`
+}
+
+func BindToStruct() {
+	var u UserInfo
+	var r requests.Request
+	res, err := r.SetUrl("").Get()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	err = res.JsonBind(&u)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%s", u.Results[0].ExtendUserID)
+}
+
+func SetTimeOut() {
+	r := requests.New()
+	res, err := r.SetUrl("http://sso.hupu.io").SetTimeOut(time.Second).Get()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("%#v\n", res)
+}
+
 func main() {
-	ChainCall()
-	StructCall()
-	StructureCall()
+	// ChainCall()
+	// StructCall()
+	// StructureCall()
+	// BindToStruct()
+	SetTimeOut()
 }
